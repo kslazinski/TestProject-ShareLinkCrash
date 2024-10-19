a5a163df3d9afa76651ce32d457f954702d6c7d5
Kris Slazinski <kris@next-planet.com>
2024-10-19 20:37:14 +0700

    Initial Commit

---
+++ ShareLinkCrash/ShareLinkCrashApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareLinkCrashApp.swift
+//  ShareLinkCrash
+//
+//  Created by Kris Slazinski on 19/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShareLinkCrashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
