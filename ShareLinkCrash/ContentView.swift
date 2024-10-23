bbc1c892d6b8fadaa07d88b5ae8bd46f415d5d31
Kris Slazinski <kris@next-planet.com>
2024-10-23 14:00:46 +0700

    Added example image

--- ShareLinkCrash/ContentView.swift
+++ ShareLinkCrash/ContentView.swift
@@ -9,13 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+//		ShareLink(item: Image(systemName: "swift"), preview: SharePreview(Text("PNG"), image: Image(systemName: "swift")))
+		ShareLink(item: Image("swift-logo"), preview: SharePreview(Text("PNG"), image: Image("swift-logo")))
+
     }
 }
 
